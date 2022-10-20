ad77700fb5c11339598625512aa2089a72c97640
Andre Avila <avila_andre14@hotmail.com>
2022-10-20 11:51:05 +0100

    Indexes and full text search sql files

---
+++ index.sql
@@ -0,0 +1,11 @@
+DROP INDEX IF EXISTS post_owner_date_idx;
+DROP INDEX IF EXISTS post_date_idx;
+DROP INDEX IF EXISTS account_tag;
+
+--Indexes
+
+CREATE INDEX account_tag ON account USING hash(account_tag);
+
+CREATE INDEX post_date_idx ON post USING btree(publication_date);
+
+CREATE INDEX post_owner_date_idx ON post USING BTREE(owner_id, publication_date);
