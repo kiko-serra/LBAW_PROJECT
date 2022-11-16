7303e1f02a18e4bb4271c8ab49bfc4faf6ea62c4
Manuel JoAPSo Gomes Alves Amorim <up202007485@fe.up.pt>
2022-11-16 21:50:08 +0000

    VSCODE favourites extension (helps organize dirs)

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes
+| Web Routes - web routes for your app
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
+
 // Home
 Route::get('/home',  function(){
     return view('home.home');
