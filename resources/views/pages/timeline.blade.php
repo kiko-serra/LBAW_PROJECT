22bec7d7d1fdcf7528eb7dca603f23c1f7f57bc1
up202006767 <up202006767@fe.up.pt>
2022-11-17 14:17:06 +0000

    Removed extra post

--- resources/views/pages/timeline.blade.php
+++ resources/views/pages/timeline.blade.php
@@ -6,11 +6,6 @@
 
 <section id="cards">
   @each('partials.post', $posts, 'post')
-  <article class="card">
-    <form class="new_card">
-      <input type="text" name="name" placeholder="new card">
-    </form>
-  </article>
 </section>
 
 @endsection
