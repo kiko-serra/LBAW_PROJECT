4adb4d4f3cb09781887ef75afd6319f15fef06d7
up202006767 <up202006767@fe.up.pt>
2022-11-22 00:50:31 +0000

    Basic user accounts

--- resources/sql/seed.sql
+++ resources/sql/seed.sql
@@ -304,8 +304,8 @@ CREATE INDEX tsv_idx ON post USING GIN(tsvectors);
 
 INSERT INTO account (account_tag, password, name, age, birthday, is_private, email, university, course, is_verified, description, location, pronouns, is_admin, is_blocked)
 VALUES
- ('AvilaAndre', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'André Ávila', 20, '2002-07-01', false, 'up202006767@edu.fe.up.pt', 'Faculdade de Engenharia da Universidade do Porto', 'Engenharia Informática e Computação', true, 'Olá, bem-vindo à minha página!', 'Porto', 'He/Him', true, false),
- ('rspencock0', '3MYlqie', 'Rickert Spencock', 16, '1983-06-03', false, 'rspencock0@java.com', 'Tarim University', 'Dakota', false, 'Peritoneal suture', 'Santa Catalina', 'Diablo', false, false),
+ ('Admin', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'Conta Admin', 20, '2002-07-01', false, 'conta@admin.pt', 'Faculdade de Engenharia da Universidade do Porto', 'Engenharia Informática e Computação', true, 'Olá, bem-vindo à minha página!', 'Porto', 'He/Him', true, false),
+ ('Utilizador', '$2y$10$HfzIhGCCaxqyaIdGgjARSuOKAcm1Uy82YfLuNaajn6JrjLWy9Sj/W', 'Jorge Silva', 16, '1983-06-03', false, 'jorge@mail.pt', 'Tarim University', 'Dakota', false, 'Peritoneal suture', 'Santa Catalina', 'He/Him', false, false),
  ('fo1', 'IKYukrzIy', 'Felike O'' Liddy', 20, '1977-03-25', false, 'fo1@wikia.com', 'Wenzhou University', 'PT Cruiser', false, 'Anterior chamber op NEC', 'Qianhong', 'Avalon', false, false),
  ('gwhilder2', 'RHgQE5FU', 'Ginny Whilder', 23, '1972-09-13', false, 'gwhilder2@technorati.com', 'Hirosaki University', 'Yukon XL 1500', true, 'Lacrimal punctum probe', 'Cipatujah', 'Rondo', true, false),
  ('nratt3', 'OWZlLLjIz', 'Nedda Ratt', 24, '1983-07-05', true, 'nratt3@weebly.com', 'Hogeschool Rotterdam', 'Pathfinder', false, 'Opn rep umb hrn-grft NEC', 'Asprángeloi', 'Fox', false, false),
