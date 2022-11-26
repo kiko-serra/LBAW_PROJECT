90dd0ffc21a8bc22b66a59ec62f41701430634e9
up202006767 <up202006767@fe.up.pt>
2022-11-26 23:12:20 +0000

    Added Ajax code from template

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -32,6 +32,24 @@ const followUser = function() {
     // TODO: send ajax request
 }
 
+function encodeForAjax(data) {
+    if (data == null) return null;
+    return Object.keys(data).map(function(k){
+      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&');
+}  
+
+function sendAjaxRequest(method, url, data, handler) {
+    let request = new XMLHttpRequest();
+
+    request.open(method, url, true);
+    request.setRequestHeader('X-CSRF-TOKEN', document.querySelector('meta[name="csrf-token"]').content);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.addEventListener('load', handler);
+    request.send(encodeForAjax(data));
+}  
+
+
 function addEventListeners() {
     let userProfileConnectionButton = document.querySelector('#userProfileConnections');
     let editUserModalBack = document.querySelector('#editUserModalBack');
