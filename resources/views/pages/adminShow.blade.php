57f70ec04801f8008379067eebfc94337dd1cb03
kiko-serra <franciscopimentelserra@gmail.com>
2022-12-05 09:52:20 +0000

    Admin page created

--- resources/views/pages/adminShow.blade.php
+++ resources/views/pages/adminShow.blade.php
@@ -5,22 +5,21 @@
 @section('content')
 <section class="content">
     <div class="text-center text-black text-6xl">User Profiles</div>
-    <div class="card-collection flex flex-col">
+    <div class="flex flex-col mt-5">
         @foreach ($users as $user)
-            <div id={{$user->id_account}} class="card-body flex flex-row m-6 ml-9">
+            <div id={{$user->id_account}} class="card-body flex flex-row m-6 text-center items-center h-28 break-words">
                 <img src="{{ asset('storage/' . $user->profile_picture) }}" class="img-thumbnail" alt="">
-                <a href="/user/{{ $user->id_account }}">{{ $user->name }}</a> 
+                <a class="underline hover:no-underline" href="/user/{{ $user->id_account }}">{{ $user->name }}</a> 
                 <p class="col-auto">{{ $user->account_tag }}</p>
                 <p class="col-auto">{{ $user->email }}</p>
                 <p class="col-auto">{{ $user->birthday }}</p>
                 <p class="col-auto">{{ $user->university }}</p>
                 <p class="col-auto">{{ $user->course }}</p>
-                <p class="is_blocked col-auto">{{$user->is_blocked}}</p>
                 
                 @if ($user->is_blocked == true)
-                    <input class="button" type="button" value="Unblock" onclick="block(this);">
+                    <input class="bg-green-400 hover:bg-green-700 rounded py-2 px-9 cursor-pointer text-center" type="button" value="Unblock" onclick="block(this);">
                 @else
-                <input class="button" type="button" value="Block" onclick="block(this);">
+                    <input class="bg-red-400 hover:bg-red-700 rounded py-2 px-9 cursor-pointer text-center" type="button" value="Block" onclick="block(this);">
                 @endif
             </div>
         @endforeach 
