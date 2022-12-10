f51fa2aaae4f7d71073d303d58dadb77467255be
up202006767 <up202006767@fe.up.pt>
2022-12-10 12:45:44 +0000

    modal visual

--- resources/views/partials/leftPanel/panel.blade.php
+++ resources/views/partials/leftPanel/panel.blade.php
@@ -68,24 +68,27 @@
 <div class="fixed flex justify-center items-center overflow-y-auto overflow-x-hidden h-screen w-screen top-0 right-0 left-0 z-50">
     <div class="absolute w-full h-full top-0 bg-black opacity-20"></div>
     <div id="left_panel_groups_create_modal" class="relative w-fit h-fit bg-white opacity-100 flex flex-col justify-start items-center p-4 rounded-lg mt-4 gap-y-4">
+        <h3 class="text-2xl -mb-3 font-semibold">Create Group</h3>
         <div class="flex flex-row gap-x-12">
             <section class="flex flex-col justify-center items-center">
-                <div class="rounded-full w-36 h-36 bg-blue-700">
-
+                <div class="group-square w-36 h-36 bg-blue-300 overflow-hidden hover:backdrop-saturate-125">
+                    <span class="flex justify-center items-center w-full h-full cursor-pointer group">
+                        <img src={{ asset('icons/edit.svg') }} alt="edit icon" width=28" height=28" class="hidden h-9 w-9 group-hover:block">
+                    </span>
                 </div>
-                <input type="file" class="w-36" name="groupimg" id="left_panel_groups_create_groupimg">
+                <input type="file" class="hidden" name="groupimg" id="left_panel_groups_create_groupimg">
             </section>
             <section class="gap-y-4"">
                 <div class="flex flex-col gap-y-1">
-                    <label for="groupdesc">Group name:</label>
+                    <label for="groupdesc" class="mt-2">Group name:</label>
                     <input type="text" placeholder="Type Here" class="text-input" name="groupname" id="left_panel_groups_create_groupname">
                 </div>
                 <div class="flex flex-col gap-y-1">
-                    <label for="groupdesc">Group description:</label>
+                    <label for="groupdesc" class="mt-2">Group description:</label>
                     <textarea name="groupdesc" placeholder="Type Here" class="textarea-input" id="left_panel_groups_create_groupdesc" cols="30" rows="3"></textarea>
                 </div>
-                <div class="w-full flex justify-start gap-x-2">
-                    <input type="checkbox" name="groupprivate" id="left_panel_groups_create_groupprivate">
+                <div class="w-full flex justify-start gap-x-2 mt-2 items-center">
+                    <input type="checkbox" name="groupprivate" class="w-5 h-5" id="left_panel_groups_create_groupprivate">
                     <label for="groupprivate">Private group:</label>
                 </div>
             </section>
