b9ba61a39b10600423a9cec83c399dafb7302478
up202006767 <up202006767@fe.up.pt>
2022-12-23 16:53:07 +0000

    Fix invite button

--- resources/views/partials/inviteGroupModal/inviteUser.blade.php
+++ resources/views/partials/inviteGroupModal/inviteUser.blade.php
@@ -1,7 +1,7 @@
 <article data-id="{{ $id }}" class="flex flex-row bg-slate-200 p-2 rounded-md items-center justify-between">
     <div class="flex flex-row items-center">
-        <div class="bg-blue-500 rounded-full w-12 h-12 items-center"></div>
-        <div class="flex flex-col ml-1 w-40 overflow-x-clip whitespace-nowrap">
+        <div class="bg-blue-500 rounded-full w-12 h-12 items-center flex-shrink-0"></div>
+        <div class="flex flex-col ml-1 w-32 overflow-x-clip whitespace-nowrap">
             <p class="text-black text-base">{{ $name }}</p>
             <p class="text-gray-600 text-sm">{{ '@' . $account_tag }}</p>
         </div>
