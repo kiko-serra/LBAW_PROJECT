6442f1e9e31a07ed95084ebae484ca90cb8f0adb
up202006767 <up202006767@fe.up.pt>
2022-12-31 14:43:15 -0100

    Recovery mail view template

--- app/Mail/PasswordRecovery.php
+++ app/Mail/PasswordRecovery.php
@@ -9,7 +9,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class PasswordRecoveryMail extends Mailable
+class PasswordRecovery extends Mailable
 {
     use Queueable, SerializesModels;
 
