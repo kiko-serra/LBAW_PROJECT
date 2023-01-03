20cbe13bc305b8607826e7cffe2abc8169ae64e4
up202006767 <up202006767@fe.up.pt>
2023-01-03 17:13:17 -0100

    Fixed background

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-  <head>
+
+<head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,29 +18,30 @@
         // Fix for Firefox autofocus CSS bug
     </script>
     <script src="https://kit.fontawesome.com/343294b271.js" crossorigin="anonymous"></script>
-    <script type="text/javascript" src={{ asset('js/app.js') }} defer>
-</script>
-  </head>
-  <body>
+    <script type="text/javascript" src={{ asset('js/app.js') }} defer></script>
+</head>
+
+<body>
     <main>
-      <area id="background_color_1"></area>
-      <header>
-        <h1><a href="{{ url('/timeline') }}">Unilinks</a></h1> <!-- Mudar link! -->
-        @if (Auth::check()) 
-        <div class="search_bar flex flex-row">
-          <!-- TODO -->
-          <i class="fa-solid fa-magnifying-glass"></i>
-          <form type= "get" action="{{url('/user_search')}}">
-            <input id="account_tag" type="text" name="account_tag" required>
-          </form>
-        </div>
-        <a class="logout_button" href="{{ url('/logout') }}"> Logout </a>
-        <a href="{{ route('profile', Auth::user()->id_account) }}">{{ Auth::user()->name }}</a>
-        @endif
-      </header>
-      <section id="content">
-        @yield('content')
-      </section>
+        <div id="background_color_1"></div>
+        <header>
+            <h1><a href="{{ url('/timeline') }}">Unilinks</a></h1> <!-- Mudar link! -->
+            @if (Auth::check())
+                <div class="search_bar flex flex-row">
+                    <!-- TODO -->
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                    <form type="get" action="{{ url('/user_search') }}">
+                        <input id="account_tag" type="text" name="account_tag" required>
+                    </form>
+                </div>
+                <a class="logout_button" href="{{ url('/logout') }}"> Logout </a>
+                <a href="{{ route('profile', Auth::user()->id_account) }}">{{ Auth::user()->name }}</a>
+            @endif
+        </header>
+        <section id="content">
+            @yield('content')
+        </section>
     </main>
-  </body>
+</body>
+
 </html>
